95bbfd91063062a7558a08beb0773361bdfd0f1f
Elky Schlesinger <elkyschlesinger@Elkys-MacBook-Air.local>
2023-02-22 19:17:32 -0500

    WordleGame with the right dictionary

--- src/main/java/weiss/wordle/WordleGame.java
+++ src/main/java/weiss/wordle/WordleGame.java
@@ -1,62 +1,47 @@
 package weiss.wordle;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Random;
 
 public class WordleGame {
-    private final ArrayList<String> dictionary = new ArrayList<String>();
+
+
+    //private final ArrayList<String> dictionary = new ArrayList<String>();
     private final Random random = new Random();
-    CharResult correct = CharResult.Correct;
-    CharResult notPresent = CharResult.NotFound;
-    CharResult wrongPlace = CharResult.WrongPlace;
-    private String correctWord;
+    private final String correctWord;
 
 
-    public WordleGame() throws FileNotFoundException {
-        File file = new File("src/main/java/weiss/wordle/dictionary.txt");
-        Scanner sc = new Scanner(file);
+    public WordleGame(WordleDictionary dictionary) throws FileNotFoundException {
 
-        while (sc.hasNextLine()) {
-            dictionary.add(sc.nextLine());
-        }
+        Random random = new Random();
 
-        ArrayList<String> words = new ArrayList<String>();
         ArrayList<String> fiveLetterWords = new ArrayList<String>();
-        String[] dictLine;
-        for (int i = 0; i < dictionary.size(); i++) {
-            dictLine = dictionary.get(i).split(" ", 2);
-            words.add(dictLine[0]);
-        }
-        for (int x = 0; x < words.size(); x++) {
-            if (words.get(x).length() == 5) {
-                fiveLetterWords.add(words.get(x));
+
+        for (int i = 0; i < dictionary.getList().size(); i++) {
+            if (dictionary.getList().get(i).length() == 5) {
+                fiveLetterWords.add(dictionary.getList().get(i));
             }
         }
         int number = random.nextInt(fiveLetterWords.size());
         correctWord = fiveLetterWords.get(number);
+    }
 
-    }
-    public String getCorrectWord(){
-        return this.correctWord;
-    }
 
     public CharResult[] guess(String guessString) {
 
         CharResult[] results = new CharResult[guessString.length()];
 
-        for(int i = 0; i < guessString.length(); i++) {
-          if (!(correctWord.contains(Character.toString(guessString.charAt(i))))){
-              results[i] = CharResult.NotFound;
-          }else if(guessString.charAt(i) == correctWord.charAt(i)){
-              results[i] = CharResult.Correct;
-            }else{
-              results[i] = CharResult.WrongPlace;
+        for (int i = 0; i < guessString.length(); i++) {
+            if (!(correctWord.contains(Character.toString(guessString.charAt(i))))) {
+                results[i] = CharResult.NotFound;
+            } else if (guessString.charAt(i) == correctWord.charAt(i)) {
+                results[i] = CharResult.Correct;
+            } else {
+                results[i] = CharResult.WrongPlace;
             }
         }
 
-    return results;
+        return results;
     }
 }
--- src/test/java/weiss/wordle/WordleDictionaryTest.java
+++ src/test/java/weiss/wordle/WordleDictionaryTest.java
@@ -1,14 +1,20 @@
 package weiss.wordle;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static weiss.wordle.CharResult.*;
 
 class WordleDictionaryTest {
     @Test
-   public void getDefinition() throws FileNotFoundException {
+    public void getDefinition() throws FileNotFoundException {
 
         WordleDictionary wordle = new WordleDictionary();
 
@@ -17,6 +23,7 @@ class WordleDictionaryTest {
         assertEquals("one [adj]", actual);
 
     }
+
     @Test
     public void getList() throws FileNotFoundException {
 
@@ -27,17 +34,40 @@ class WordleDictionaryTest {
         assertEquals(167964, total);
 
     }
+
     @Test
     public void guess() throws FileNotFoundException {
+        //given
+        WordleDictionary dictionary = Mockito.mock(WordleDictionary.class);
+        ArrayList<String> words = new ArrayList<>(List.of("APPLE"));
+        doReturn(words).when(dictionary).getList();
+        WordleGame game = new WordleGame(dictionary);
 
-        WordleGame game = new WordleGame();
+        //when
+        CharResult[] results = game.guess("APPLE");
 
-        CharResult[] results = game.guess(game.getCorrectWord());
-
-        assertArrayEquals(new String[]{"correct", "correct", "correct", "correct", "correct"}, results);
+        //then
+        CharResult[] answer = {Correct, Correct, Correct, Correct, Correct};
+        assertArrayEquals(answer, results);
 
     }
 
+    @Test
+    public void guessTwo() throws FileNotFoundException {
+        //given
+        WordleDictionary dictionary = Mockito.mock(WordleDictionary.class);
+        ArrayList<String> words = new ArrayList<>(List.of("APPLE"));
+        doReturn(words).when(dictionary).getList();
+        WordleGame game = new WordleGame(dictionary);
+
+        //when
+        CharResult[] results = game.guess("SUPER");
+
+        //then
+        CharResult[] answer = {NotFound, NotFound, Correct, WrongPlace, NotFound};
+        assertArrayEquals(answer, results);
+
+    }
 
 
 }
