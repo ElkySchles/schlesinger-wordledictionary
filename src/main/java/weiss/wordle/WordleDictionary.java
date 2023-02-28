23fb22ab5804b1b7ce2e9cf8324bf36ea6a722b8
Elky Schlesinger <elkyschlesinger@Elkys-MacBook-Air.local>
2023-02-28 17:38:17 -0500

    Added split to constructor

--- src/main/java/weiss/wordle/WordleDictionary.java
+++ src/main/java/weiss/wordle/WordleDictionary.java
@@ -6,37 +6,42 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordleDictionary {
-    //ArrayList to store all the lines in the dictionary
-    private final ArrayList<String> dictionary = new ArrayList<String>();
-    //private String definition;
-    private final ArrayList<String> justWords = new ArrayList<String>();
+
+    ArrayList<String> justWords;
+
+    ArrayList<String> definition;
 
 
     public WordleDictionary() throws FileNotFoundException {
+        justWords = new ArrayList<>();
+        definition = new ArrayList<>();
+
+        final ArrayList<String> dictionary = new ArrayList<>();
         File file = new File("src/main/java/weiss/wordle/dictionary.txt");
         Scanner sc = new Scanner(file);
 
         while (sc.hasNextLine()) {
             dictionary.add(sc.nextLine());
         }
-
+        for (int i = 0; i < dictionary.size(); i++) {
+            String[] dictLine = dictionary.get(i).split(" ", 2);
+            if (dictLine.length != 1) {
+                justWords.add(dictLine[0]);
+                definition.add(dictLine[1]);
+            } else {
+                justWords.add(dictLine[0]);
+                definition.add("");
+            }
+        }
     }
 
 
     //GetDefiniton takes in a word and returns the definition
     public String getDefinition(String word) {
-
-        for (int i = 0; i < dictionary.size(); i++) {
-
-            String[] dictLine = dictionary.get(i).split(" ", 2);
-
-
-            if (word.equalsIgnoreCase((dictLine[0]))) {
-                if (dictLine.length != 1) {
-                    return dictLine[1];
-                } else {
-                    return "";
-                }
+        word = word.toUpperCase();
+        for (int i = 0; i < justWords.size(); i++) {
+            if (word.equals(justWords.get(i))) {
+                return definition.get(i);
             }
         }
         return null;
@@ -44,12 +49,7 @@ public class WordleDictionary {
 
     //GetList returns only the words from the dictionary
     public ArrayList<String> getList() {
-        String[] dictLine;
 
-        for (int i = 0; i < dictionary.size(); i++) {
-            dictLine = dictionary.get(i).split(" ", 2);
-            justWords.add(dictLine[0]);
-        }
         return justWords;
     }
 
